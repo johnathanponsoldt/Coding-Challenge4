75a833aaaaec4b5572ffb5ae9de52beb4413c26e
johnathanponsoldt <jponsoldt772@gmail.com>
2024-09-15 22:19:10 -0400

    Initialize inventory with product objects

---
+++ inventory_management.js
@@ -0,0 +1,15 @@
+// Task 1: Create an Inventory Array of Product Objects
+
+const inventory = [
+
+        { name: 'Football', price: 35, quantity: 10, lowStockLevel: 3 },
+    
+        { name: 'SoccerBall', price: 50, quantity: 5, lowStockLevel: 2 },
+    
+        { name: 'Basketball', price: 40, quantity: 7, lowStockLevel: 1 },
+    
+        { name: 'Baseball', price: 5, quantity: 15, lowStockLevel: 5 },
+    
+        { name: 'Volleyball', price: 30, quantity: 3, lowStockLevel: 1 }
+    
+];
